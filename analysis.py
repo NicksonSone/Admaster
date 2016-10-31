d56727a1c24fec6b87a1705becf50e3ad11ffd0c
nickson <875186147@qq.com>
2016-10-31 22:02:01 +0800

    new analysis on data

--- analysis.py
+++ analysis.py
@@ -1,16 +1,64 @@
-import csv as csv 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function   # log output
 import numpy as np
+import pandas as pd
 
-# initialize data
-file = csv.reader(open("train"), delimiter="	")
-line = 500000
-data = []
+def log_output(filename, content, head=None):
+    with open(filename, "a") as log:
+        if head != None:
+            print(head + '\n', file=log)
+        print(content, file=log)
+        
 
-for i in xrange(line):
-	row = file.next()
-	data.append(row)
-data = np.array(data)
+columns = ['rank', 'dt', 'cookie', 'ip', 'mobile_idfa', 'mobile_imei', 'mobile_android_id', 
+	      'mobile_openudid', 'mobile_mac', 'timestamps', 'camp_id', 'creativeid', 'mobile_os', 
+	      'mobile_type','mobile_app_key', 'mobile_app_name', 'placement_id', 'user_agent', 
+	      'media_id', 'os', 'born_time', 'flag']
 
-# proportion of fake access
-fake_access = np.sum(data[0::, 21].astype(np.float)) # the last field is 21. 1 for fake access, 0 for normal one
-proportion_fake = fake_access / np.float(line) # the proportion is 0.0324759998
+# initialize data  
+# it is faster to use pandas.read_csv to load large dataset
+## chunks = pd.read_csv("training_set", iterator=True, delimiter="	", chunksize=100000)
+## training_set = pd.concat([chunk for chunk in chunks], ignore_index=True)
+## training_set.columns = columns
+
+# proportion of fake access in training set: 0.49
+## percentage_fake = fake_access / np.float(line) 
+
+# check missing values
+training_size = len(training_set.index)
+log_output('log', training_size , header='training size')
+## nulls = (training_size - training_set.count()) / training_size
+## log_output("log", head="proportion of missing values", content=nulls)
+
+# proportion of different sources in all views
+androids = 1 - np.isnan(training_set['mobile_android_id'])
+cnt_android = np.sum(androids)
+
+mobile_idfa = 1 - np.isnan(training_set['mobile_idfa'])
+mobile_openudid = 1 - np.isnan(training_set['mobile_openudid'])
+cnt_ios = np.sum(npmobile_idfa + mobile_openudid)
+
+webs = 1 - np.isnan(training_set['cookie'])
+cnt_web = np.sum(webs)
+
+log_output('log', cnt_android, header='androids')
+log_output('log', cnt_ios, header='ios')
+log_output('log', cnt_web, header='web')
+
+
+# categorize fake accesses based on source
+# proportion of sources in fake access
+# The time when most access occurs
+
+
+# i wanna check if most of fake access originate from a few devices and burst of fake access tend to cluster
+# in certain amount of time
+
+	# time and device id 
+	# how to encode categorical data to reflect the heirachy
+
+# if yes, repetited access will be a crucial feature for classification
+# otherwise, i can randomly subsample data from training
+
---
+++ preprocess.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+import csv as csv
+import numpy as np
+import pandas as pd
+
+train = pd.read_csv('train.csv')
+
+log_column = ['rank', 'dt', 'cookie', 'ip', 'mobile_idfa', 'mobile_imei', 'mobile_android_id', 
+	      'mobile_openudid', 'mobile_mac', 'timestamps', 'camp_id', 'creativeid', 'mobile_os', 
+	      'mobile_type','mobile_app_key', 'mobile_app_name', 'placement_id', 'user_agent', 
+	      'media_id', 'os', 'born_time', 'flag']
+
+media_column = ['mediaid', 'media_id', 'Category', 'firstType_cn', 'secondType_cn', 'tag'] 
+
+# insert code to get a list of categorical columns into a variable say categorical_columns
+# insert code to take care of the missing values in the columns in whatever way you like to
+# but is is important that missing values are replaced.
+
+# Get the categorical values into a 2D numpy array
+train_categorical_values = np.array(train[categorical_columns])
+
+# OneHotEncoder will only work on integer categorical values, so if you have strings in your
+# categorical columns, you need to use LabelEncoder to convert them first
+
+# do the first column
+enc_label = LabelEncoder()
+train_data = enc_label.fit_transform(train_categorcial_values[:,0])
+
+# do the others
+for i in range(1, train_categorical_values.shape[1]):
+	enc_label = LabelEncoder()
+	train_data = np.column_stack((train_data, enc_label.fit_transform(train_categorical_values[:,i])))
+
+train_categorical_values = train_data.astype(float)
+
+# if you have only integers then you can skip the above part from do the first column and uncomment the following line
+# train_categorical_values = train_categorical_values.astype(float) 
+
+enc_onehot = OneHotEncoder()
+train_cat_data = enc.fit_transform(train_categorical_values)
+
+# play around and print enc.n_values_ features_indices_ to see how many unique values are there in each column
+
+# create a list of columns to help create a DF from np array
+# so say if you have col1 and col2 as the categorical columns with 2 and 3 unique values respectively. The following code
+# will give you col1_0, col1_1, col2_1,col2_2,col2_3 as the columns
+
+cols = [categorical_columns[i] + '_' + str(j) for i in range(0,len(categorical_columns)) for j in range(0,enc.n_values_[i]) ]
+train_cat_data_df = pd.DataFrame(train_cat_data.toarray(),columns=cols)
+
+# get this columns back into the data frame
+train[cols] = train_cat_data_df[cols]
+
+# append the target column. Obviously rename it to whatever is your target column 
+cols.append('target')
+# So now you have a dataframe with only the categorical columns and the target. You can now do whatever you want to do with it :)
+train_cat_df = train[cols]
