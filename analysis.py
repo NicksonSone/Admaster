8557a0b708479d21919de8589a57e43f5d54253e
NicksonSone <875186147@qq.com>
2016-10-31 22:07:43 +0800

    remove bug

--- analysis.py
+++ analysis.py
@@ -10,21 +10,21 @@ def log_output(filename, content, head=None):
         if head != None:
             print(head + '\n', file=log)
         print(content, file=log)
-        
 
-columns = ['rank', 'dt', 'cookie', 'ip', 'mobile_idfa', 'mobile_imei', 'mobile_android_id', 
-	      'mobile_openudid', 'mobile_mac', 'timestamps', 'camp_id', 'creativeid', 'mobile_os', 
-	      'mobile_type','mobile_app_key', 'mobile_app_name', 'placement_id', 'user_agent', 
-	      'media_id', 'os', 'born_time', 'flag']
 
-# initialize data  
+columns = ['rank', 'dt', 'cookie', 'ip', 'mobile_idfa', 'mobile_imei', 'mobile_android_id',
+'mobile_openudid', 'mobile_mac', 'timestamps', 'camp_id', 'creativeid', 'mobile_os',
+'mobile_type','mobile_app_key', 'mobile_app_name', 'placement_id', 'user_agent',
+'media_id', 'os', 'born_time', 'flag']
+
+# initialize data
 # it is faster to use pandas.read_csv to load large dataset
-## chunks = pd.read_csv("training_set", iterator=True, delimiter="	", chunksize=100000)
-## training_set = pd.concat([chunk for chunk in chunks], ignore_index=True)
-## training_set.columns = columns
+chunks = pd.read_csv("training_set", iterator=True, delimiter="	", chunksize=100000)
+training_set = pd.concat([chunk for chunk in chunks], ignore_index=True)
+training_set.columns = columns
 
 # proportion of fake access in training set: 0.49
-## percentage_fake = fake_access / np.float(line) 
+## percentage_fake = fake_access / np.float(line)
 
 # check missing values
 training_size = len(training_set.index)
@@ -33,14 +33,14 @@ log_output('log', training_size , header='training size')
 ## log_output("log", head="proportion of missing values", content=nulls)
 
 # proportion of different sources in all views
-androids = 1 - np.isnan(training_set['mobile_android_id'])
+androids = 1 - pd.isnull(training_set['mobile_android_id'])
 cnt_android = np.sum(androids)
 
-mobile_idfa = 1 - np.isnan(training_set['mobile_idfa'])
-mobile_openudid = 1 - np.isnan(training_set['mobile_openudid'])
-cnt_ios = np.sum(npmobile_idfa + mobile_openudid)
+mobile_idfa = 1 - pd.isnull(training_set['mobile_idfa'])
+mobile_openudid = 1 - pd.isnull(training_set['mobile_openudid'])
+cnt_ios = np.sum(mobile_idfa + mobile_openudid)
 
-webs = 1 - np.isnan(training_set['cookie'])
+webs = 1 - pd.isnull(training_set['cookie'])
 cnt_web = np.sum(webs)
 
 log_output('log', cnt_android, header='androids')
@@ -56,7 +56,7 @@ log_output('log', cnt_web, header='web')
 # i wanna check if most of fake access originate from a few devices and burst of fake access tend to cluster
 # in certain amount of time
 
-	# time and device id 
+	# time and device id
 	# how to encode categorical data to reflect the heirachy
 
 # if yes, repetited access will be a crucial feature for classification
